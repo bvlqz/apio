f8df74a400b65d447e7044c63296052a5deca7d9
Jorge Alberto Becerra <jorge.bvlqz@gmail.com>
2022-02-24 21:16:30 -0800

    Add save button, edit save path

--- src/editor.cpp
+++ src/editor.cpp
@@ -7,16 +7,56 @@ Editor::Editor()
 
 void Editor::draw()
 {
-    static bool show_text_editor = true; 
     static ImGuiInputTextFlags flags = ImGuiInputTextFlags_AllowTabInput;
+    static ImGuiWindowFlags windowFlags = 0;
+    static bool userWantsEditPath = false;
 
-    ImGui::Begin("Text Editor", &show_text_editor); 
-    ImGui::InputText("File Name", pathBuf, IM_ARRAYSIZE(pathBuf));
+    windowFlags |= ImGuiWindowFlags_NoCollapse;
+    windowFlags |= ImGuiWindowFlags_MenuBar;
+    windowFlags |= ImGuiWindowFlags_NoTitleBar;
+
+    ImGui::Begin("Text Editor", NULL, windowFlags); 
+
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.KeyCtrl || io.KeySuper)
+    {
+        if (ImGui::IsKeyDown(ImGuiKey_S)) save();
+    }
+
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Save", "Ctrl+S")) {save();}
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Edit"))
+        {
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+    }
+
     // ImGui::Text("Keys mods: %s%s%s%s", io.KeyCtrl ? "CTRL " : "", io.KeyShift ? "SHIFT " : "", io.KeyAlt ? "ALT " : "", io.KeySuper ? "SUPER " : "");
-    ImGui::InputTextMultiline("##source", contentBuf, IM_ARRAYSIZE(contentBuf), ImVec2(-FLT_MIN, -FLT_MIN - 100), flags);
+    ImGui::InputTextMultiline("##source", contentBuf, IM_ARRAYSIZE(contentBuf), ImVec2(-FLT_MIN, -FLT_MIN - ImGui::GetTextLineHeight() - 10), flags);
 
+    ImGui::Text("%s", permissions); 
 
-    if (ImGui::Button("Save")) save();
+    if (userWantsEditPath)
+    {
+        ImGui::InputText("##FileName", pathBuf, IM_ARRAYSIZE(pathBuf));
+        ImGui::SameLine();
+        if (ImGui::Button("OK"))
+        {
+            userWantsEditPath = false;
+        }
+    } else {
+        ImGui::Text("%s", pathBuf);
+        if (ImGui::IsMouseDoubleClicked(0))
+        {
+            userWantsEditPath = true;
+        }
+    }
 
     ImGui::End();
 }
